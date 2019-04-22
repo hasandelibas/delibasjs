52254aa057a607cec6aa733f23fb2b04526eaf52
Hasan Delibaş <hasan.delibas94@gmail.com>
2019-04-22 16:07:53 +0300

    Update delibas.js

--- delibas.js
+++ delibas.js
@@ -46,3 +46,41 @@ function nval(variable, index, def) {
 	}
 }
 nval.def = null;
+
+
+/*
+	Front End Code Brocker
+*/
+function Broker(val,key,isJsonKey){
+    if(isJsonKey==null)
+        isJsonKey = false;
+    if(typeof(val)=="object"){
+        var ret =  key + " = {";
+        if(isJsonKey==true)
+            ret =  key + " : {";
+
+        var keys = Object.keys(val);
+        for (let i = 0; i < keys.length; i++) {
+            const element = keys[i];
+            ret += Broker(val[element],element,true)
+        }
+        ret += "}"
+        return ret;
+    }
+    if(typeof(val)=="string"){
+        if(isJsonKey==true)
+            return key + " : \""+  val.replaceAll("\"","\\\"") + ","
+        return key + " = \""+  val.replaceAll("\"","\\\"")
+
+    }
+    if(typeof(val)=="number"){
+        if(isJsonKey==true)
+            return key + " : " + val + ",";
+        return key + " = " + val;
+    }
+    if(typeof(val)=="function"){
+        if(isJsonKey==true)
+            return key + " : " + val.toString() + ","
+        return key + " = " + val.toString()
+    }
+}
